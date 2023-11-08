1c75e27efd5712e73bf0bcbf0c4aae072098f92f
Adriel Tayag <adriel.tayag.cics@ust.edu.ph>
2023-11-08 23:13:33 +0800

    Initial

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index(){
+        $categories=Category::all();
+        return view('admin.category.category',compact('categories'));
+    }
+    public function store(Request $request)
+    {
+        $category = new Category();
+        $category->category_name = $request->input('category_name');
+        $category->user_id = Auth::id();
+
+        $category->save();
+
+        return redirect()->route('AllCat');
+    }
+}
